9117b7d57e0f120ca8503a95d7bd8bf0c2f0b93b
mendonjr <prasadmendon744@gmail.com>
2022-07-22 16:41:11 +0530

    Sending first GET request using RestAssured

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# api-automation
+# API Testing using Rest-Assured Library
+
+Testing and validation of REST services in Java is harder than in dynamic languages such as Ruby and Groovy. REST Assured brings the simplicity of using these languages into the Java domain.
--- src/test/java/apitesting/SendGETRequest.java
+++ src/test/java/apitesting/SendGETRequest.java
@@ -1,11 +1,38 @@
 package apitesting;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+
 import static io.restassured.RestAssured.*;
 import static io.restassured.matcher.RestAssuredMatchers.*;
 import static org.hamcrest.Matchers.*;
 
 public class SendGETRequest {
+    /*
+     Set Authentication - Basic Auth / Bearer token using Headers
 
+     *Request ---> Set of request specifications
+
+     * Response - Do validation
+
+     */
     public static void main(String[] args) {
-        given().auth().basic("wdwdwdw", "dwdwddwd");
+       Response response =  given().auth().basic("sk_test_51LNAdjSG0BCeOo6hpaenzUvn73Q3zr0m3GN4jGypPPNvGCe0S0Fqv0b0M2koy49BTM74N4hVm6dRJylfDSo5naHp00ASvjyn3F", "")
+                .get("https://api.stripe.com/v1/customers");
+
+      //  response.prettyPrint();
+        String jsonresponse = response.asString();
+        //System.out.println(jsonresponse);
+        System.out.println("Response code is : " +response.statusCode());
+
+
+        /* Different ways of specifying content type in headers
+       // given().contentType(ContentType.JSON);
+       // given().contentType("application/JSON");
+       // given().header("content-type", "application/JSON").auth().basic(username,password);
+       */
+
+
+
+
     }
 }
